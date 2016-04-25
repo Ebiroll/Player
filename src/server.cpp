a2c4ccd4487e39ce362491659f6af45f8dd6449e
Olof Astrand <olof.astrand@gmail.com>
2016-04-25 06:33:21 +0000

    Fixed error

--- src/server.cpp
+++ src/server.cpp
@@ -56,8 +56,8 @@ void PageRequestHandler::handleRequest(HTTPServerRequest& request, HTTPServerRes
       
       std::string req=request.getURI();
       printf("------------- Serving web page: %s\n",req.c_str());
-      if ( request.getURI()  == "data/lib/jquery-2.2.3.min.js") {
-          filename="/lib/jquery-2.2.3.min.js";
+      if ( request.getURI()  == "lib/jquery-2.2.3.min.js") {
+          filename="data/lib/jquery-2.2.3.min.js";
           handleFileRequest(request, response);
           printf("FILE\n");
 	    return;
